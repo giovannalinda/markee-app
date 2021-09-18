e38505978c1f11de8ff1e4015a03f0c6af999d10
Giovanna Linda <euGiovannaSouza@gmail.com>
2021-09-18 19:05:28 -0300

    settings and cleaning

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,5 @@
-import styled, { css } from 'styled-components/macro'
-import { ReactNode } from 'react'
-
 export function App () {
   return (
-  // <Title>Markee</Title>,
-    <Title lala='a'>Markee</Title>
+    <h1>Markee</h1>
   )
 }
-
-type TitleProps = {
-  children: ReactNode
-  lala: string
-}
-/*
-function Title ({ children }: TitleProps) {
-  return <h1>{children}</h1>
-}
-*/
-
-const Title = styled.h1<TitleProps>`${({ theme }) => css`
-  background: ${theme.colors.black};
-  color: ${theme.colors.primary};
-`}`
--- src/root.tsx
+++ src/root.tsx
@@ -1,29 +1,8 @@
-import { ThemeProvider, createGlobalStyle } from 'styled-components'
 import { App } from 'app'
-import { theme } from './@types/theme'
+import 'normalize.css'
 
 export function Root () {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <App />
   )
 }
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    box-sizing: 0;
-  }
-
-  html {
-    font-size: 62.5%;
-  }
-
-  body {
-    font-family: 'DM Sans', sans-serif;
-  }
-`
